ce9a297518dec5c360840d6f1612789df96d627e
Vishal9066 <vishalkumar.8193@gmail.com>
2020-03-19 21:51:07 +0530

    completed placeholder.py

--- placeholder.py
+++ placeholder.py
@@ -4,4 +4,4 @@ sent = "%s is 5 years old" #with a placeholder
 print(sent%variable) #printing the string with a variable
 print(sent%"Kumar") #printing the string with a value
 sent = "%s is %d years old" #with string and int placeholders
-print(sent%("vishal",5))#passing the values
+print(sent%("vishal",5))#passing the values.
