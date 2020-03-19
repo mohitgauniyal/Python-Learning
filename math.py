259d1a18288446363746f2f5495868e4ce74c091
Vishal9066 <vishalkumar.8193@gmail.com>
2020-03-19 21:28:38 +0530

    completed math.py

--- math.py
+++ math.py
@@ -1,2 +1,14 @@
 #!bin/bash
-print(100 + 50) #add
+print(100 + 50)
+a = 10
+b = 20
+print(a+b) #add,sub,mul,div,%
+c = "hello"
+d = "vishal"
+print(c + " "+ d) # concetnation and string addition
+print(50*50/50/50) #(PEMDAS) not bodmas
+print("add" * 4) #multiple strings
+print(c[0])  #only 0th index will be printed.
+print(c[4])  #only 4th index will be printed.
+print(c[0:3]) #from 0 to 2 index will be printed.
+print(c[0:-1]) # only last character not printed.
