2941a48a990b89bb7c9e87abe298b084f3759843
Vishal9066 <vishalkumar.8193@gmail.com>
2020-03-29 14:04:00 +0530

    variables and methods

--- variables_and_methods.py
+++ variables_and_methods.py
@@ -3,4 +3,4 @@ quote = "Everything is good." #variable
 print(quote)
 print(quote.upper()) #Uppercase method
 print(quote.lower()) #lowercase method
-print(quote.title()) #Tilecase method
+print(quote.title()) #Titlecase method
