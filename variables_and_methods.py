41a8e8abc895d921ac3c135bd8c55ab79224b8b4
Vishal9066 <vishalkumar.8193@gmail.com>
2020-03-29 14:39:38 +0530

    variables and methods

--- variables_and_methods.py
+++ variables_and_methods.py
@@ -4,3 +4,18 @@ print(quote)
 print(quote.upper()) #Uppercase method
 print(quote.lower()) #lowercase method
 print(quote.title()) #Titlecase method
+
+print(len(quote)) #length
+
+name = "Vishal" #string
+age = 24 #int
+gpa = 7.1  #float
+
+print("My name is " + name + " & my age is " + str(age) + " years old" + " with " + str(gpa) + " GPA." )
+
+age += 1
+print(age)
+
+birthday = 2
+age += birthday
+print(age)
