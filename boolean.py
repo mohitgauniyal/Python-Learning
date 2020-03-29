03067d5bc68c8fcfdd2be9a8cf069f82900e2c44
Vishal9066 <vishalkumar.8193@gmail.com>
2020-03-29 21:52:22 +0530

    Boolean Expressions

---
+++ boolean.py
@@ -0,0 +1,12 @@
+#Boolean Expressions
+
+print("Boolean Expression")
+
+bool1 = True      #true
+bool2 = 3*3 == 9  #check its true or not
+bool3 = False     #false
+bool4 = 3*3 != 9  #check its true or not
+
+print(bool1,bool2,bool3,bool4)
+print(type(bool2)) #type of var. i.e. bool
+print(type("bool1")) #type of var. i.e. str
