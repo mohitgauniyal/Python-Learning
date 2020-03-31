8ce4f06631d3301bcbd393476094f3a04cbc05f6
Vishal9066 <vishalkumar.8193@gmail.com>
2020-03-31 21:11:30 +0530

    Loops
    Signed-off-by: Vishal9066 <vishalkumar.8193@gmail.com>

---
+++ Loops.py
@@ -0,0 +1,11 @@
+#for loops : start to finish of an iterate
+fruits = ["Apple","Mango","Banana"]
+
+for juices in fruits:
+    print(juices)
+
+#while loops:Execute as long as true
+i = 0
+while i < 10:
+    print(i)
+    i += 1
