706cc86ecb3e9227b07313e9be4be5a8a6c4a1b6
Vishal9066 <vishalkumar.8193@gmail.com>
2020-03-31 21:17:48 +0530

    Math

--- math.py
+++ math.py
@@ -7,7 +7,7 @@ c = "hello"
 d = "vishal"
 print(c + " "+ d) # concetnation and string addition
 print(50*50/50/50) #(PEMDAS) not bodmas
-print("add" * 4) #multiple strings
+print("add" * 4) #multiple strings.
 print(c[0])  #only 0th index will be printed.
 print(c[4])  #only 4th index will be printed.
 print(c[0:3]) #from 0 to 2 index will be printed.
