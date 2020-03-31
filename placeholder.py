494b93e237ef5ee8205528feba8d5f57906a6a0c
Vishal9066 <vishalkumar.8193@gmail.com>
2020-03-31 21:16:59 +0530

    Placeholder

--- placeholder.py
+++ placeholder.py
@@ -3,5 +3,5 @@ print(variable + " is 5 years old") #with concatenation
 sent = "%s is 5 years old" #with a placeholder
 print(sent%variable) #printing the string with a variable
 print(sent%"Kumar") #printing the string with a value
-sent = "%s is %d years old" #with string and int placeholders
+sent = "%s is %d years old" #with string and int placeholders.
 print(sent%("vishal",5))#passing the values.
